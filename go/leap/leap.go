cd32b3fb363115343d9b97ae00d0e295cb53318e
Pradip Patil <pradippatil@users.noreply.github.com>
2016-12-05 23:37:55 -0500

    .

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,12 +1,15 @@
-// Leap stub file
-
-// The package name is expected by the test program.
 package leap
 
-// testVersion should match the targetTestVersion in the test file.
 const testVersion = 2
 
-// It's good style to write a comment here documenting IsLeapYear.
-func IsLeapYear(int) bool {
-	// Write some code here to pass the test suite.
+func IsLeapYear(year int) bool {
+
+	if year%100 != 0 && year%4 == 0 {
+		return true
+	}
+	if year%100 == 0 && year%400 == 0 {
+		return true
+	}
+	return false
+
 }
