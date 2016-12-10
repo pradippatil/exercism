a25e4d638b8189afd115143acf73ae671d099d3b
Pradip Patil <pradippatil@users.noreply.github.com>
2016-12-10 07:54:30 -0500

    .

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -3,13 +3,8 @@ package leap
 const testVersion = 2
 
 func IsLeapYear(year int) bool {
-
-	if year%100 != 0 && year%4 == 0 {
-		return true
-	}
-	if year%100 == 0 && year%400 == 0 {
+	if (year%100 != 0 && year%4 == 0) || year%400 == 0 {
 		return true
 	}
 	return false
-
 }
