780270eb03446f26d296bc4f158cc1b9ce375cd9
Pradip Patil <pradippatil@users.noreply.github.com>
2016-12-10 08:14:11 -0500

    .

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,7 +1,21 @@
 package hamming
 
+import "errors"
+
 const testVersion = 5
 
 func Distance(a, b string) (int, error) {
-	// your code here!
+
+	var c int
+	lengthErr := errors.New("Strings length mismatch")
+
+	if len(a) != len(b) {
+		return 0, lengthErr
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			c++
+		}
+	}
+	return c, nil
 }
