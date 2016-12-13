252c9ed6c33f825089cf3237a00b1bd58849a6ab
Pradip Patil <pradippatil@users.noreply.github.com>
2016-12-12 22:40:45 -0500

    .

--- go/triangle/triangle.go
+++ go/triangle/triangle.go
@@ -1,17 +1,58 @@
 package triangle
 
-const testVersion = 2
+import (
+	"math"
+	"sort"
+)
 
-// Code this function.
-func KindFromSides(a, b, c float64) Kind
+const testVersion = 3
 
-// Notice it returns this type.  Pick something suitable.
-type Kind
+type Kind struct {
+	name string
+}
 
-// Pick values for the following identifiers used by the test program.
-NaT // not a triangle
-Equ // equilateral
-Iso // isosceles
-Sca // scalene
+var NaT = Kind{"NaT"}
+var Iso = Kind{"Iso"}
+var Sca = Kind{"Sca"}
+var Equ = Kind{"Equ"}
 
-// Organize your code for readability.
+// KindFromSides returns type of triangle for given sides
+func KindFromSides(a, b, c float64) Kind {
+
+	// Check for Not-a-Number and Infinity values
+	if math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c) || math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0) {
+		return NaT
+	}
+
+	// Triangle inequality theorem (sum of two sides should be greater than third)
+	// Better to sort the sides by length, so that we can just compare sum of smallest pair
+	// Exception here is a 'line' is allowed (sum of two sides equal to third)
+	var sides sort.Float64Slice = []float64{a, b, c}
+	sides.Sort()
+	if sides[0]+sides[1] < sides[2] {
+		return NaT
+	}
+
+	// Check for -ve values
+	if a <= 0 || b <= 0 || c <= 0 {
+		return NaT
+	}
+
+	// If all sides are equal
+	if a == b && b == c {
+		return Equ
+	}
+
+	// If any two sides are equal
+	if a == b || b == c || c == a {
+		return Iso
+	}
+
+	// If none of the sides are equal
+	if a != b && b != c && c != a {
+		return Sca
+	}
+
+	return NaT
+
+}
